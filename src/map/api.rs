fc1e715291e6949d924aefa6f479dac57dd41955
Al Liu <scygliu1@gmail.com>
2024-05-17 00:00:42 +0800

    cleanup

---
+++ src/map/api.rs
@@ -0,0 +1,363 @@
+use super::*;
+
+impl<T, C> SkipMap<T, C> {
+  /// Returns the height of the highest tower within any of the nodes that
+  /// have ever been allocated as part of this skiplist.
+  #[inline]
+  pub fn height(&self) -> u32 {
+    self.arena.height()
+  }
+
+  /// Returns the number of bytes that have allocated from the arena.
+  #[inline]
+  pub fn size(&self) -> usize {
+    self.arena.size()
+  }
+
+  /// Returns the number of remaining bytes can be allocated by the arena.
+  #[inline]
+  pub fn remaining(&self) -> usize {
+    self.arena.remaining()
+  }
+
+  /// Returns the capacity of the arena.
+  #[inline]
+  pub const fn capacity(&self) -> usize {
+    self.arena.capacity()
+  }
+
+  /// Returns the number of entries in the skipmap.
+  #[inline]
+  pub fn len(&self) -> usize {
+    self.arena.len() as usize
+  }
+
+  /// Returns true if the skipmap is empty.
+  #[inline]
+  pub fn is_empty(&self) -> bool {
+    self.len() == 0
+  }
+
+  /// Returns the maximum version of all entries in the map.
+  #[inline]
+  pub fn max_version(&self) -> u64 {
+    self.arena.max_version()
+  }
+
+  /// Returns the minimum version of all entries in the map.
+  #[inline]
+  pub fn min_version(&self) -> u64 {
+    self.arena.min_version()
+  }
+
+  /// Returns the comparator used to compare keys.
+  #[inline]
+  pub const fn comparator(&self) -> &C {
+    &self.cmp
+  }
+
+  /// Flushes outstanding memory map modifications to disk.
+  ///
+  /// When this method returns with a non-error result,
+  /// all outstanding changes to a file-backed memory map are guaranteed to be durably stored.
+  /// The file's metadata (including last modification timestamp) may not be updated.
+  #[cfg(all(feature = "memmap", not(target_family = "wasm")))]
+  #[cfg_attr(docsrs, doc(cfg(all(feature = "memmap", not(target_family = "wasm")))))]
+  pub fn flush(&self) -> std::io::Result<()> {
+    self.arena.flush()
+  }
+
+  /// Asynchronously flushes outstanding memory map modifications to disk.
+  ///
+  /// This method initiates flushing modified pages to durable storage, but it will not wait for
+  /// the operation to complete before returning. The file's metadata (including last
+  /// modification timestamp) may not be updated.
+  #[cfg(all(feature = "memmap", not(target_family = "wasm")))]
+  #[cfg_attr(docsrs, doc(cfg(all(feature = "memmap", not(target_family = "wasm")))))]
+  pub fn flush_async(&self) -> std::io::Result<()> {
+    self.arena.flush_async()
+  }
+
+  #[cfg(all(test, feature = "std"))]
+  #[inline]
+  pub(crate) fn with_yield_now(mut self) -> Self {
+    self.yield_now = true;
+    self
+  }
+}
+
+impl SkipMap {
+  /// Create a new skipmap according to the given capacity
+  ///
+  /// **Note:** The capacity stands for how many memory allocated,
+  /// it does not mean the skiplist can store `cap` entries.
+  ///
+  ///
+  ///
+  /// **What the difference between this method and [`SkipMap::mmap_anon`]?**
+  ///
+  /// 1. This method will use an `AlignedVec` ensures we are working within Rust's memory safety guarantees.
+  ///   Even if we are working with raw pointers with `Box::into_raw`,
+  ///   the backend ARENA will reclaim the ownership of this memory by converting it back to a `Box`
+  ///   when dropping the backend ARENA. Since `AlignedVec` uses heap memory, the data might be more cache-friendly,
+  ///   especially if you're frequently accessing or modifying it.
+  ///
+  /// 2. Where as [`SkipMap::mmap_anon`] will use mmap anonymous to require memory from the OS.
+  ///   If you require very large contiguous memory regions, `mmap` might be more suitable because
+  ///   it's more direct in requesting large chunks of memory from the OS.
+  ///
+  /// [`SkipMap::mmap_anon`]: #method.mmap_anon
+  pub fn new(cap: usize) -> Result<Self, Error> {
+    Self::with_comparator(cap, Ascend)
+  }
+
+  /// Create a new skipmap according to the given capacity, and mmaped to a file.
+  ///
+  /// **Note:** The capacity stands for how many memory mmaped,
+  /// it does not mean the skipmap can store `cap` entries.
+  ///
+  /// `lock`: whether to lock the underlying file or not
+  #[cfg(all(feature = "memmap", not(target_family = "wasm")))]
+  #[cfg_attr(docsrs, doc(cfg(all(feature = "memmap", not(target_family = "wasm")))))]
+  pub fn mmap_mut<P: AsRef<std::path::Path>>(
+    path: P,
+    open_options: OpenOptions,
+    mmap_options: MmapOptions,
+  ) -> std::io::Result<Self> {
+    Self::mmap_mut_with_comparator(path, open_options, mmap_options, Ascend)
+  }
+
+  /// Open an exist file and mmap it to create skipmap.
+  ///
+  /// `lock`: whether to lock the underlying file or not
+  #[cfg(all(feature = "memmap", not(target_family = "wasm")))]
+  #[cfg_attr(docsrs, doc(cfg(all(feature = "memmap", not(target_family = "wasm")))))]
+  pub fn mmap<P: AsRef<std::path::Path>>(
+    path: P,
+    open_options: OpenOptions,
+    mmap_options: MmapOptions,
+  ) -> std::io::Result<Self> {
+    Self::mmap_with_comparator(path, open_options, mmap_options, Ascend)
+  }
+
+  /// Create a new skipmap according to the given capacity, and mmap anon.
+  ///
+  /// **What the difference between this method and [`SkipMap::new`]?**
+  ///
+  /// 1. This method will use mmap anonymous to require memory from the OS directly.
+  ///   If you require very large contiguous memory regions, this method might be more suitable because
+  ///   it's more direct in requesting large chunks of memory from the OS.
+  ///
+  /// 2. Where as [`SkipMap::new`] will use an `AlignedVec` ensures we are working within Rust's memory safety guarantees.
+  ///   Even if we are working with raw pointers with `Box::into_raw`,
+  ///   the backend ARENA will reclaim the ownership of this memory by converting it back to a `Box`
+  ///   when dropping the backend ARENA. Since `AlignedVec` uses heap memory, the data might be more cache-friendly,
+  ///   especially if you're frequently accessing or modifying it.
+  ///
+  /// [`SkipMap::new`]: #method.new
+  #[cfg(all(feature = "memmap", not(target_family = "wasm")))]
+  #[cfg_attr(docsrs, doc(cfg(all(feature = "memmap", not(target_family = "wasm")))))]
+  pub fn mmap_anon(mmap_options: MmapOptions) -> std::io::Result<Self> {
+    Self::mmap_anon_with_comparator(mmap_options, Ascend)
+  }
+}
+
+impl<T, C> SkipMap<T, C> {
+  /// Like [`SkipMap::new`], but with a custom comparator.
+  pub fn with_comparator(cap: usize, cmp: C) -> Result<Self, Error> {
+    let arena = Arena::new_vec(cap, Node::<T>::min_cap(), Node::<T>::alignment() as usize);
+    Self::new_in(arena, cmp, false)
+  }
+
+  /// Like [`SkipMap::mmap_mut`], but with a custom comparator.
+  #[cfg(all(feature = "memmap", not(target_family = "wasm")))]
+  #[cfg_attr(docsrs, doc(cfg(all(feature = "memmap", not(target_family = "wasm")))))]
+  pub fn mmap_mut_with_comparator<P: AsRef<std::path::Path>>(
+    path: P,
+    open_options: OpenOptions,
+    mmap_options: MmapOptions,
+    cmp: C,
+  ) -> std::io::Result<Self> {
+    let alignment = Node::<T>::alignment() as usize;
+    let min_cap = Node::<T>::min_cap();
+    let arena = Arena::mmap_mut(path, open_options, mmap_options, min_cap, alignment)?;
+    Self::new_in(arena, cmp, false).map_err(invalid_data)
+  }
+
+  /// Like [`SkipMap::mmap`], but with a custom comparator.
+  #[cfg(all(feature = "memmap", not(target_family = "wasm")))]
+  #[cfg_attr(docsrs, doc(cfg(all(feature = "memmap", not(target_family = "wasm")))))]
+  pub fn mmap_with_comparator<P: AsRef<std::path::Path>>(
+    path: P,
+    open_options: OpenOptions,
+    mmap_options: MmapOptions,
+    cmp: C,
+  ) -> std::io::Result<Self> {
+    let alignment = Node::<T>::alignment() as usize;
+    let min_cap = Node::<T>::min_cap();
+    let arena = Arena::mmap(path, open_options, mmap_options, min_cap, alignment)?;
+    Self::new_in(arena, cmp, true).map_err(invalid_data)
+  }
+
+  /// Like [`SkipMap::mmap_anon`], but with a custom comparator.
+  #[cfg(all(feature = "memmap", not(target_family = "wasm")))]
+  #[cfg_attr(docsrs, doc(cfg(all(feature = "memmap", not(target_family = "wasm")))))]
+  pub fn mmap_anon_with_comparator(mmap_options: MmapOptions, cmp: C) -> std::io::Result<Self> {
+    let alignment = Node::<T>::alignment() as usize;
+    let min_cap = Node::<T>::min_cap();
+    let arena = Arena::new_anonymous_mmap(mmap_options, min_cap, alignment)?;
+    Self::new_in(arena, cmp, false).map_err(invalid_data)
+  }
+
+  /// Clear the skiplist to empty and re-initialize.
+  ///
+  /// # Safety
+  /// This mehod is not concurrency safe, invokers must ensure that no other threads are accessing the skipmap.
+  pub unsafe fn clear(&mut self) -> Result<(), Error> {
+    if self.ro {
+      return Err(Error::Readonly);
+    }
+
+    let head = Node::new_empty_node_ptr(&self.arena)
+      .expect("arena is not large enough to hold the head node");
+    let tail = Node::new_empty_node_ptr(&self.arena)
+      .expect("arena is not large enough to hold the tail node");
+
+    // Safety:
+    // We will always allocate enough space for the head node and the tail node.
+    unsafe {
+      // Link all head/tail levels together.
+      for i in 0..MAX_HEIGHT {
+        let head_link = head.tower(&self.arena, i);
+        let tail_link = tail.tower(&self.arena, i);
+        head_link.next_offset.store(tail.offset, Ordering::Relaxed);
+        tail_link.prev_offset.store(head.offset, Ordering::Relaxed);
+      }
+    }
+
+    self.head = head;
+    self.tail = tail;
+    self.arena.clear();
+    Ok(())
+  }
+}
+
+
+impl<T: Trailer, C: Comparator> SkipMap<T, C> {
+  /// Returns true if the key exists in the map.
+  #[inline]
+  pub fn contains_key<'a, 'b: 'a>(&'a self, version: u64, key: &'b [u8]) -> bool {
+    self.get(version, key).is_some()
+  }
+
+  /// Returns the first entry in the map.
+  pub fn first(&self, version: u64) -> Option<EntryRef<'_, T, C>> {
+    self.first_in(version).map(|n| EntryRef::from_node(n, self))
+  }
+
+  /// Returns the last entry in the map.
+  pub fn last(&self, version: u64) -> Option<EntryRef<'_, T, C>> {
+    self.last_in(version).map(|n| EntryRef::from_node(n, self))
+  }
+
+  /// Returns the value associated with the given key, if it exists.
+  pub fn get<'a, 'b: 'a>(&'a self, version: u64, key: &'b [u8]) -> Option<EntryRef<'a, T, C>> {
+    unsafe {
+      let (n, eq) = self.find_near(version, key, false, true); // findLessOrEqual.
+
+      let n = n?;
+      let node = n.as_ptr();
+      let node_key = node.get_key(&self.arena);
+
+      if eq {
+        return node.get_value(&self.arena).map(|val| EntryRef {
+          map: self,
+          key: node_key,
+          trailer: node.trailer,
+          value: Some(val),
+        });
+      }
+
+      if !matches!(self.cmp.compare(key, node_key), cmp::Ordering::Equal) {
+        return None;
+      }
+
+      if node.trailer.version() > version {
+        return None;
+      }
+
+      node.get_value(&self.arena).map(|val| EntryRef {
+        map: self,
+        key: node_key,
+        trailer: node.trailer,
+        value: Some(val),
+      })
+    }
+  }
+
+  /// Returns an `EntryRef` pointing to the highest element whose key is below the given bound.
+  /// If no such element is found then `None` is returned.
+  pub fn upper_bound<'a, 'b: 'a>(
+    &'a self,
+    version: u64,
+    upper: Bound<&'b [u8]>,
+  ) -> Option<EntryRef<'a, T, C>> {
+    match upper {
+      Bound::Included(key) => self.le(version, key).map(|n| EntryRef::from_node(n, self)),
+      Bound::Excluded(key) => self.lt(version, key).map(|n| EntryRef::from_node(n, self)),
+      Bound::Unbounded => self.last(version),
+    }
+  }
+
+  /// Returns an `EntryRef` pointing to the lowest element whose key is above the given bound.
+  /// If no such element is found then `None` is returned.
+  pub fn lower_bound<'a, 'b: 'a>(
+    &'a self,
+    version: u64,
+    lower: Bound<&'b [u8]>,
+  ) -> Option<EntryRef<'a, T, C>> {
+    match lower {
+      Bound::Included(key) => self.ge(version, key).map(|n| EntryRef::from_node(n, self)),
+      Bound::Excluded(key) => self.gt(version, key).map(|n| EntryRef::from_node(n, self)),
+      Bound::Unbounded => self.first(version),
+    }
+  }
+
+  /// Returns a new iterator, this iterator will yield the latest version of all entries in the map less or equal to the given version.
+  #[inline]
+  pub const fn iter(&self, version: u64) -> iterator::MapIterator<T, C> {
+    iterator::MapIterator::new(version, self, false)
+  }
+
+  /// Returns a new iterator, this iterator will yield all versions for all entries in the map less or equal to the given version.
+  #[inline]
+  pub const fn iter_all_versions(&self, version: u64) -> iterator::MapIterator<T, C> {
+    iterator::MapIterator::new(version, self, true)
+  }
+
+  /// Returns a iterator that within the range, this iterator will yield the latest version of all entries in the range less or equal to the given version.
+  #[inline]
+  pub fn range<'a, Q, R>(&'a self, version: u64, range: R) -> iterator::MapIterator<'a, T, C, Q, R>
+  where
+    &'a [u8]: PartialOrd<Q>,
+    Q: ?Sized + PartialOrd<&'a [u8]>,
+    R: RangeBounds<Q> + 'a,
+  {
+    iterator::MapIterator::range(version, self, range, false)
+  }
+
+  /// Returns a iterator that within the range, this iterator will yield all versions for all entries in the range less or equal to the given version.
+  #[inline]
+  pub fn range_all_versions<'a, Q, R>(
+    &'a self,
+    version: u64,
+    range: R,
+  ) -> iterator::MapIterator<'a, T, C, Q, R>
+  where
+    &'a [u8]: PartialOrd<Q>,
+    Q: ?Sized + PartialOrd<&'a [u8]>,
+    R: RangeBounds<Q> + 'a,
+  {
+    iterator::MapIterator::range(version, self, range, true)
+  }
+}
